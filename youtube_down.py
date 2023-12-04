22696e436224eabda73c74d9027c699c86401e22
Victor <vhstavares@gmail.com>
2023-12-04 11:32:18 -0300

    update

--- youtube_down.py
+++ youtube_down.py
@@ -1,29 +1,42 @@
 from pytube import YouTube
 from moviepy.editor import VideoFileClip
+import os
+import requests
 
-def download_video(url, output_path = 'downloads_videos'):
+def download_video(url, output_path='downloads_videos'):
     try:
         youtube = YouTube(url)
         video = youtube.streams.get_highest_resolution()
         video.download(output_path)
-        print(f"Vídeo baixado com sucesso em: {output_path}\{video.default_filename}")
-        return  f".\{output_path}\{video.default_filename}"
+        print(f"Vídeo baixado com sucesso em: {os.path.join(output_path, video.default_filename)}")
+        return os.path.join(output_path, video.default_filename)
     except Exception as e:
         print(f"Erro ao baixar o vídeo: {e}")
         return None
 
-def convert_to_audio(video_path, audio_output_file = 'output_audio.mp3'):
+def convert_to_audio(video_path, audio_output_file='output_audio.mp3'):
     try:
+        audio_dir = 'audios'
+        if not os.path.exists(audio_dir):
+            os.makedirs(audio_dir)
+
         video_clip = VideoFileClip(video_path)
         audio_clip = video_clip.audio
-        audio_clip.write_audiofile(audio_output_file)
-        print(f"Áudio convertido com sucesso: {audio_output_file}")
+        audio_clip.write_audiofile(os.path.join(audio_dir, audio_output_file))
+        print(f"Áudio convertido com sucesso: {os.path.join(audio_dir, audio_output_file)}")
     except Exception as e:
         print(f"Erro ao converter para áudio: {e}")
 
 if __name__ == "__main__":
-    video_url = input("Insira url: ")
-    downloaded_video_path = download_video(video_url)
-    if downloaded_video_path:
-        file_name = downloaded_video_path.split("\\")[-1].split(".")[0]
+    video_url = input("Insira a URL do vídeo: ")
+
+    if requests.get(video_url).status_code == 200:
+        print("O servidor está disponível.")
+        downloaded_video_path = download_video(video_url)
+        if downloaded_video_path:
+            file_name = os.path.basename(downloaded_video_path).split(".")[0]
+
         convert_to_audio(downloaded_video_path, file_name + ".mp3")
+
+    else:
+        print("O servidor está indisponível.")
